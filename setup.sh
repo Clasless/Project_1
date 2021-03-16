07ad9f2b50f0aa2dc8820bf7f063eb30a775dc6c
Clasless <58229896+Clasless@users.noreply.github.com>
2021-03-15 21:27:57 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "AIcrowd installer starting..."
+
+if ! nvidia-smi > /dev/null 2>&1; then
+  echo "🚫 No GPU runtime detected..."
+  echo "Please update the runtime by going to Runtime -> Change Runtime Type -> Hardware Accelerator -> GPU"
+  exit 1
+fi
+
+echo "Setting up the environment for you!"
+
+echo "⚙️ Installing PyTorch..."
+pip install -U torch==1.5 torchvision==0.6 -f https://download.pytorch.org/whl/cu101/torch_stable.html > /dev/null
+
+echo "⚙️ Installing COCO API..."
+pip install cython pyyaml==5.1 > /dev/null
+pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI' > /dev/null
+
+echo "⚙️ Installing detectron..."
+pip install detectron2==0.1.2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/index.html > /dev/null
+
+
+#Datasets (This gets downloaded in "/content/foodChallenge/data")
+echo "🗄 Preparing the dataset for training..."
+wget -q -nv https://datasets.aicrowd.com/default/aicrowd-public-datasets/food-recognition-challenge/v0.4/train-v0.4.tar.gz
+unzip -q -d dataset train-v0.4.tar.gz
+
+echo "🗄 Preparing the validation dataset..."
+wget -q -nv https://datasets.aicrowd.com/default/aicrowd-public-datasets/food-recognition-challenge/v0.4/val-v0.4.tar.gz
+unzip -q -d dataset val-v0.4.tar.gz
+
+echo "All set! 🎉🍻"
